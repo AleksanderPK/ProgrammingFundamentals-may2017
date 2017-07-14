5fe78db0de1a0cd927ba8e14417b42e29ef743b2
Aleksander Kochev <aleksander_k@abv.bg>
2017-07-14 20:21:45 +0300

    ProgrammingFundamentalsListsExercises

--- U12_Lists-Exercises/E01_MaxSequenceOfEqualElements/E01_MaxSequenceOfEqualElements.cs
+++ U12_Lists-Exercises/E01_MaxSequenceOfEqualElements/E01_MaxSequenceOfEqualElements.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace E01_MaxSequenceOfEqualElements
 {
@@ -10,6 +8,50 @@ namespace E01_MaxSequenceOfEqualElements
     {
         static void Main(string[] args)
         {
+            var input = new List<int> { 5, 5, 5,5,5,5,5, 2,2,2,2,2,2,2, 1, 1, 1, 1, 1, 1 }; //Console.ReadLine().Split(' ').Select(dig => int.Parse(dig));
+
+            var container = new List<int>();
+            var result = new List<int>();
+            var counter = 1;
+            var maxCounter = 0;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                var currentDig = input[i];
+                container.Add(currentDig);
+
+                for (int j = i + 1; j < input.Count; j++)
+                {
+                    if (currentDig == input[j])
+                    {
+                        counter++;
+                        container.Add(input[j]);
+
+                        if (counter > maxCounter)
+                        {
+                            maxCounter = counter;
+                            result = new List<int>(container);
+                        }                        
+                    }
+                    else
+                    {
+                        counter = 1;
+                        i = j - 1;
+                        break;
+                    }
+
+                    i = j - 1;
+                }
+                
+                container.Clear();
+            }
+
+            foreach (var elm in result) 
+            {
+                Console.Write(elm + " ");
+            }
+
+
         }
     }
 }
