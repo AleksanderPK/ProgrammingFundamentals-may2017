500b86b77aed6cdf45cc11e53914f0fa07229b14
Aleksander Kochev <aleksander_k@abv.bg>
2017-07-14 21:47:35 +0300

    Add_E01_MaxSequenceOfEqualElements

--- U12_Lists-Exercises/E01_MaxSequenceOfEqualElements/E01_MaxSequenceOfEqualElements.cs
+++ U12_Lists-Exercises/E01_MaxSequenceOfEqualElements/E01_MaxSequenceOfEqualElements.cs
@@ -8,50 +8,39 @@ namespace E01_MaxSequenceOfEqualElements
     {
         static void Main(string[] args)
         {
-            var input = new List<int> { 5, 5, 5,5,5,5,5, 2,2,2,2,2,2,2, 1, 1, 1, 1, 1, 1 }; //Console.ReadLine().Split(' ').Select(dig => int.Parse(dig));
+            var input = Console.ReadLine()
+                .Split(' ')
+                .Select(dig => int.Parse(dig))
+                .ToList<int>();            
 
-            var container = new List<int>();
             var result = new List<int>();
-            var counter = 1;
-            var maxCounter = 0;
+            var container = new List<int>();
 
-            for (int i = 0; i < input.Count; i++)
+            for (int i = 0, j = 0; i < input.Count; i = j)
             {
-                var currentDig = input[i];
-                container.Add(currentDig);
+                container.Add(input[i]);
 
-                for (int j = i + 1; j < input.Count; j++)
+                for (j = i + 1; j < input.Count; j++)
                 {
-                    if (currentDig == input[j])
+                    if (input[i] == input[j])
                     {
-                        counter++;
                         container.Add(input[j]);
-
-                        if (counter > maxCounter)
-                        {
-                            maxCounter = counter;
-                            result = new List<int>(container);
-                        }                        
                     }
                     else
                     {
-                        counter = 1;
-                        i = j - 1;
                         break;
-                    }
-
-                    i = j - 1;
+                    }                                        
                 }
-                
+
+                if (container.Count > result.Count)
+                {
+                    result = new List<int>(container);
+                }
+
                 container.Clear();
             }
 
-            foreach (var elm in result) 
-            {
-                Console.Write(elm + " ");
-            }
-
-
+            result.ForEach(x => Console.Write(x + " "));
         }
     }
 }
