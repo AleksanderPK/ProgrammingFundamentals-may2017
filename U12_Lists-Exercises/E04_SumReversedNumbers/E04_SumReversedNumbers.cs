7733c909744d79eaa293bdc4792cd18fc158a22c
Aleksander Kochev <aleksander_k@abv.bg>
2017-07-15 16:02:40 +0300

    Add E04_SumReversedNumbers

---
+++ U12_Lists-Exercises/E04_SumReversedNumbers/E04_SumReversedNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E04_SumReversedNumbers
+{
+    class E04_SumReversedNumbers
+    {
+        static void Main(string[] args)
+        {
+            var numList = Console.ReadLine().Split(' ').ToList();
+            var sum = 0;
+
+            foreach (var digit in numList)
+            {
+                var reversedDig = Reverse(digit);
+                sum += int.Parse(reversedDig);
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        public static string Reverse(string num)
+        {
+            char[] arr = num.ToCharArray();
+            Array.Reverse(arr);
+            return new String(arr);
+        }
+    }
+}
