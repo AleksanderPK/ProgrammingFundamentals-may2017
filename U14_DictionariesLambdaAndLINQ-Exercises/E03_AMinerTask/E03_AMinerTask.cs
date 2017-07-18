4c069df01ceeecb52ba93bdc7902565b7377039a
Aleksander Kochev <aleksander_k@abv.bg>
2017-07-18 19:55:22 +0300

    Add_E03_AMinerTask

---
+++ U14_DictionariesLambdaAndLINQ-Exercises/E03_AMinerTask/E03_AMinerTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E03_AMinerTask
+{
+    class E03_AMinerTask
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var container = new List<string>();
+            var resources = new Dictionary<string, string>();
+
+            while (input != "stop")
+            {
+                container.Add(input);
+                input = Console.ReadLine();                
+            }
+
+            for (int i = 0; i < container.Count; i+= 2)
+            {
+                resources[container[i]] = container[i + 1];
+            }
+
+            foreach (var kvp in resources)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
